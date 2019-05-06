2d188d7b53486cf5428a60c72df1cc3be05aba24
Abimael Espinal <abimaelespinal@hotmail.com>
2019-05-06 19:37:02 -0400

    TESTING search and contains

--- BinarySearchTree/BinarySearchTreeTest.js
+++ BinarySearchTree/BinarySearchTreeTest.js
@@ -4,7 +4,7 @@ class Node{
     constructor(data){
         this.data = data;
         this.left = null;
-        this.right = right;
+        this.right = null;
     }
 
     insert(data){
@@ -21,12 +21,21 @@ class Node{
 
     constains(data){
         if(data === this.data){
-            return this;
+            return true;
         } else if (data > this.data && this.right){
             return this.right.constains(data);
         } else if(data < this.data && this.left){
             return this.left.constains(data);
         }
-        return null;
+        return false;
     }
 }
+
+const test = new Node(50);
+
+test.insert(40);
+console.log(test.left.data)
+//prints 40
+
+console.log(test.constains(40));
+//return true
