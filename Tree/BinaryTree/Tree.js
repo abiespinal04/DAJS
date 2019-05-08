6f9e5993d7350f024e596552a857fc61bf7a0194
Abimael Espinal <abimaelespinal@hotmail.com>
2019-05-08 02:35:00 -0400

    levelWidth problem using BF traverse

--- Tree/BinaryTree/Tree.js
+++ Tree/BinaryTree/Tree.js
@@ -40,6 +40,28 @@ class Tree{
             fn(node)
         }
     }
+
+    levelWidth(){
+        const arr = [this.root, 's'];
+        const counters = [0];
+
+        while(arr.length > 1){
+            const node = arr.shift();
+
+            if(node === 's'){
+                counters.push(0);
+                arr.push('s');
+            }else{
+                arr.push(...node.children);
+                counters[counters.length -1]++;
+            }
+
+        }
+
+        return counters;
+
+    }
+
 }
 
 module.exports = {Node,Tree};
